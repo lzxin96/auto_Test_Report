7e994f0184feaabecef1e359dc9f628153dba6be
lzxin96 <545634702@qq.com>
2022-06-26 22:06:00 +0800

    调用鼠标方法

--- TestScript/test_advanced_application.py
+++ TestScript/test_advanced_application.py
@@ -1,1 +1,23 @@
 # _*_ coding:utf-8 _*_
+from selenium import webdriver
+from Util.Mouse_Simulation import Simulate_Mouse
+from Util import BrowserController
+import unittest
+import time
+
+class Test_Advanced_Application(unittest.TestCase):
+    def setUp(self):
+        self.url = 'http://www.baidu.com'
+
+
+    def test_simulate_mouse(self):
+        '''
+        调用模拟鼠标方法
+        @return:
+        '''
+        chr_driver = webdriver.Chrome()
+        chr_driver.maximize_window()
+        chr_driver.get(self.url)
+        element = chr_driver.find_element_by_id('s-usersetting-top')
+        Simulate_Mouse(chr_driver).right_click(element)
+        time.sleep(5)
